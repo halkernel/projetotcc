b696e241f7e7b9adf632f073a433967480b32ff6
Kennet Emerson <kennet.emerson@gmail.com>
2016-10-16 17:47:32 -0300

    I just started to develop the page which will compare two schools.

--- src/main/java/bean/DetalheEscolaBean.java
+++ src/main/java/bean/DetalheEscolaBean.java
@@ -12,11 +12,15 @@ import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
 
-import dao.EscolaTaxaDAO;
 import dao.EscolaDAO;
+import dao.EscolaTaxaDAO;
 import entity.Escola;
 import entity.EscolaTaxa;
 
+/**
+ * @author halkernel
+ *
+ */
 @ManagedBean
 @SessionScoped
 public class DetalheEscolaBean {
@@ -24,7 +28,9 @@ public class DetalheEscolaBean {
 	private Escola escola = new Escola();
 	private EscolaTaxa escolaTaxa = new EscolaTaxa();
 	private int idEscola;
-	private BarChartModel chartEscola;	
+	private BarChartModel chartEducacaoInfantil;
+	private BarChartModel chartEnsinoFundamental;
+	private BarChartModel chartEnsinoMedio;
     
 	
 	@PostConstruct
@@ -32,13 +38,23 @@ public class DetalheEscolaBean {
 				
 	}
 	
-	public BarChartModel preencheModelo(){
-		BarChartModel escolaModelo = new BarChartModel();
+	public BarChartModel preencheModeloEducacaoInfantil(){
+		BarChartModel educacaoInfantilModelo = new BarChartModel();
 		ChartSeries series = new ChartSeries();
-		EscolaTaxaDAO daotest = new EscolaTaxaDAO();
-		escolaTaxa = daotest.list(escola.getId());
 		series.setLabel(escolaTaxa.getTipoTaxa().getTaxaNome());
+		series.set("Creche",  escolaTaxa.getCreche());
 		series.set("Pré-Escola", escolaTaxa.getPreEscola());
+		series.set("Total", escolaTaxa.getTotalInfantil());		
+		educacaoInfantilModelo.addSeries(series);
+		return educacaoInfantilModelo;
+	}
+	public BarChartModel preencheModeloEnsinoFundamental(){
+		BarChartModel ensinoFundamentalModelo = new BarChartModel();
+		ChartSeries series = new ChartSeries();
+		
+		series.setLabel(escolaTaxa.getTipoTaxa().getTaxaNome());
+		series.set("Anos Iniciais", escolaTaxa.getPrimeiroAoQuinto());
+		series.set("Anos Finais", escolaTaxa.getSextoAoNono());
 		series.set("1º Ano", escolaTaxa.getPrimeiroAnoFundamental());
 		series.set("2º Ano", escolaTaxa.getSegundoAnoFundamental());
 		series.set("3º Ano", escolaTaxa.getTerceiroAnoFundamental());
@@ -47,19 +63,46 @@ public class DetalheEscolaBean {
 		series.set("6º Ano", escolaTaxa.getSextoAnoFundamental());
 		series.set("7º Ano", escolaTaxa.getSetimoAnoFundamental());
 		series.set("8º Ano", escolaTaxa.getOitavoAnoFundamental());		
-		series.set("9º Ano", escolaTaxa.getNonoAnoFundamental());		
-		System.out.println(escolaTaxa.getTipoTaxa().getTaxaNome()); 
-		
-		escolaModelo.addSeries(series);
-		return escolaModelo;
-	}
+		series.set("9º Ano", escolaTaxa.getNonoAnoFundamental());
+		series.set("Turmas Unificadas", escolaTaxa.getTurmasUnificadas());		
+		System.out.println(escolaTaxa.getTipoTaxa().getTaxaNome()); 		
+		ensinoFundamentalModelo.addSeries(series);
+		return ensinoFundamentalModelo;
+	}	
+	public BarChartModel preencheModeloEnsinoMedio(){
+		BarChartModel ensinoMedioModelo = new BarChartModel();
+		ChartSeries series = new ChartSeries();
+		series.setLabel(escolaTaxa.getTipoTaxa().getTaxaNome());
+		series.set("1º Ano", escolaTaxa.getPrimeiroAnoMedio());
+		series.set("2º Ano", escolaTaxa.getSegundoAnoMedio());
+		series.set("3º Ano", escolaTaxa.getTerceiroAnoMedio());
+		series.set("4º Ano", escolaTaxa.getQuartoAnoMedio());
+		series.set("Medio Não Seriado", escolaTaxa.getMedioNaoSeriado());		
+		ensinoMedioModelo.addSeries(series);
+		return ensinoMedioModelo;
+	}	
 	
-	public void criaModelo(){
-		chartEscola = preencheModelo();
-		chartEscola.setTitle(escolaTaxa.getTipoTaxa().getTaxaNome());
-		chartEscola.setLegendPosition("ne");
-		
-		Axis yAxis = chartEscola.getAxis(AxisType.Y);
+	public void criaModeloEducacaoInfantil(){
+		chartEducacaoInfantil = preencheModeloEducacaoInfantil();
+		chartEducacaoInfantil.setTitle("Educação Infantil");
+		chartEducacaoInfantil.setLegendPosition("ne");
+		Axis yAxis = chartEducacaoInfantil.getAxis(AxisType.Y);
+		yAxis.setMin(0);
+		yAxis.setMax(100);
+	}
+	public void criaModeloEnsinoFundamental(){
+		chartEnsinoFundamental = preencheModeloEnsinoFundamental();
+		chartEnsinoFundamental.setTitle("Ensino Fundamental");
+		chartEnsinoFundamental.setLegendPosition("ne");
+		Axis yAxis = chartEnsinoFundamental.getAxis(AxisType.Y);
+		yAxis.setMin(0);
+		yAxis.setMax(100);
+	}
+	public void criaModeloEnsinoMedio(){
+		chartEnsinoMedio = preencheModeloEnsinoMedio();
+		chartEnsinoMedio.setTitle("Ensino Médio");
+		chartEnsinoMedio.setLegendPosition("ne");
+		Axis yAxis = chartEnsinoMedio.getAxis(AxisType.Y);
 		yAxis.setMin(0);
 		yAxis.setMax(100);
 	}
@@ -70,44 +113,50 @@ public class DetalheEscolaBean {
 		idEscola = Integer.parseInt(params.get("escola"));
 		EscolaDAO escolaDao = new EscolaDAO();
 		escola = escolaDao.listById(idEscola);
+		EscolaTaxaDAO daotest = new EscolaTaxaDAO();
+		escolaTaxa = daotest.list(escola.getId());
 		System.out.println(idEscola);
-		criaModelo();
+		criaModeloEducacaoInfantil();
+		criaModeloEnsinoFundamental();
+		criaModeloEnsinoMedio();
 	}
-		
-	
-	
-	
-	
+			
 	public Escola getEscola() {
 		return escola;
 	}
-
 	public void setEscola(Escola escola) {
 		this.escola = escola;
 	}
-
 	public int getIdEscola() {
 		return idEscola;
 	}
-
 	public void setIdEscola(int idEscola) {
 		this.idEscola = idEscola;
 	}
 
-
-	public BarChartModel getChartEscola() {
-		return chartEscola;
+	public BarChartModel getChartEducacaoInfantil() {
+		return chartEducacaoInfantil;
 	}
 
-
-	public void setChartEscola(BarChartModel chartEscola) {
-		this.chartEscola = chartEscola;
+	public void setChartEducacaoInfantil(BarChartModel chartEducacaoInfantil) {
+		this.chartEducacaoInfantil = chartEducacaoInfantil;
 	}
 
-	
-	
+	public BarChartModel getChartEnsinoFundamental() {
+		return chartEnsinoFundamental;
+	}
 
-	
+	public void setChartEnsinoFundamental(BarChartModel chartEnsinoFundamental) {
+		this.chartEnsinoFundamental = chartEnsinoFundamental;
+	}
+
+	public BarChartModel getChartEnsinoMedio() {
+		return chartEnsinoMedio;
+	}
+
+	public void setChartEnsinoMedio(BarChartModel chartEnsinoMedio) {
+		this.chartEnsinoMedio = chartEnsinoMedio;
+	}
 	
 
 }
