f2feb9d90629e2c5befbc7f2a6d1a1cacaea8750
Kennet Emerson <kennet.emerson@gmail.com>
2016-10-29 19:12:08 -0300

    I've started programming the advanced search

--- src/main/java/bean/BuscaAvancadaBean.java
+++ src/main/java/bean/BuscaAvancadaBean.java
@@ -1,15 +1,16 @@
 package bean;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
+import org.primefaces.context.RequestContext;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.event.UnselectEvent;
 import org.primefaces.model.DualListModel;
@@ -21,18 +22,20 @@ import util.ConverteString;
 @ManagedBean
 @ViewScoped
 public class BuscaAvancadaBean {
-	
+
 	private DualListModel<Escola> escolasEscolha = new DualListModel<>();
 	private List<Escola> escolas;
 	private String nomeEscola;
+	private List<Escola> escolasSource = new ArrayList<Escola>();
+	private List<Escola> escolasTarget = new ArrayList<Escola>();
 
-     
-    @PostConstruct
-    public void init() {
 
-     }
-    
-    public DualListModel<Escola> getEscolasEscolha() {
+	@PostConstruct
+	public void init() {
+
+	}
+
+	public DualListModel<Escola> getEscolasEscolha() {
 		return escolasEscolha;
 	}
 
@@ -45,34 +48,42 @@ public class BuscaAvancadaBean {
 		nomeEscola = ConverteString.removeAcento(nomeEscola);
 		nomeEscola = nomeEscola.toUpperCase();
 		System.out.println(nomeEscola);
-		escolas = escolaDao.listByName(nomeEscola);	
-		
-		
-        List<Escola> escolasSource = new ArrayList<Escola>();
-        List<Escola> escolasTarget = new ArrayList<Escola>();
-               
-        
-        escolasSource.addAll(escolas);         
-        escolasEscolha = new DualListModel<Escola>(escolasSource, escolasTarget);
-		
-		
-    }
-    
-    
-    public void onSelect(SelectEvent event) {
-        FacesContext context = FacesContext.getCurrentInstance();
-        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Selected", event.getObject().toString()));
-    }
-     
-    public void onUnselect(UnselectEvent event) {
-        FacesContext context = FacesContext.getCurrentInstance();
-        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Unselected", event.getObject().toString()));
-    }
-     
-    public void onReorder() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "List Reordered", null));
-    }
+		escolas = escolaDao.listByName(nomeEscola);					                    
+
+		escolasSource.addAll(escolas);         
+		escolasEscolha = new DualListModel<Escola>(escolasSource, escolasTarget);
+
+
+	}
+	
+	public void checaSelecao(){
+		System.err.println("passou aki 35");
+
+	}
+
+	public void checaEscolasSelecionadas(){		
+		int i = 9;
+		i++;
+		RequestContext requestContext = RequestContext.getCurrentInstance();
+		requestContext.openDialog("PF('dlg2').show()");
+		System.out.println("passou aki 35");
+	}
+
+
+	public void onSelect(SelectEvent event) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Selected", event.getObject().toString()));
+	}
+
+	public void onUnselect(UnselectEvent event) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Unselected", event.getObject().toString()));
+	}
+
+	public void onReorder() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "List Reordered", null));
+	}
 
 
 
@@ -92,6 +103,24 @@ public class BuscaAvancadaBean {
 		this.nomeEscola = nomeEscola;
 	}
 
+	public List<Escola> getEscolasSource() {
+		return escolasSource;
+	}
 
-    
+	public void setEscolasSource(List<Escola> escolasSource) {
+		this.escolasSource = escolasSource;
+	}
+
+	public List<Escola> getEscolasTarget() {
+		return escolasTarget;
+	}
+
+	public void setEscolasTarget(List<Escola> escolasTarget) {
+		this.escolasTarget = escolasTarget;
+	}
+
+
+
+
+
 }
